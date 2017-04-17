aedff95d44b952054f43f12be17bacb517f30f97
eloaverona <efrancav@macalester.edu>
2017-04-16 21:54:07 -0500

    test

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 var myApp = angular.module('myApp', []);
+
+myApp.component('eventThumbnails', {
+	templateUrl: 'eventThumbnail.template.html',
+	controller: function($scope) {
+		var events = [{
+			image: "eventImage.png",
+			title: "The greatest event ever",
+			location: "St. Paul",
+			organizer: "Sarah Lee",
+			going: 100,
+		}, {
+			image: "eventImage.png",
+			title: "2nd greatest event ever",
+			location: "Minneapolis",
+			organizer: "Marylou",
+			going: 92,
+		}];
+		
+		$scope.events = events;
+		
+		
+	}
+});
